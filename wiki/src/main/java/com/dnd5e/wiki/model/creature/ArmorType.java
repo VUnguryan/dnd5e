9cd8e608f376683b87161c7ce6148031d0c035bf
VUnguryan <VUnguryan@VUnguryan.kiev.luxoft.com>
2018-06-15 10:15:10 +0300

    update enums

--- wiki/src/main/java/com/dnd5e/wiki/model/creature/ArmorType.java
+++ wiki/src/main/java/com/dnd5e/wiki/model/creature/ArmorType.java
@@ -1,5 +1,21 @@
 package com.dnd5e.wiki.model.creature;
 
-public enum ArmorType {
-
+public enum ArmorType
+{
+  LEATHER("кожанный доспех"),
+  NATURAL("природный доспех"),  
+  PLATE("латы"),
+  WRECKAGE("");
+  
+  private String cyrillicName;
+  
+  private ArmorType(String cyrillicName)
+  {
+    this.cyrillicName = cyrillicName;
+  }
+  
+  public String getCyrillicName()
+  {
+    return this.cyrillicName;
+  }
 }
--- wiki/src/main/java/com/dnd5e/wiki/model/creature/MonsterType.java
+++ wiki/src/main/java/com/dnd5e/wiki/model/creature/MonsterType.java
@@ -1,5 +1,33 @@
 package com.dnd5e.wiki.model.creature;
 
 public enum MonsterType {
-
+	  ABERRATION("Аберация"),  
+	  ANIMAL("Зверь"),
+	  CELESTIAL("Небожитель"),
+	  CONSTRUCT("Конструкт"),
+	  DRAGON("Дракон"),
+	  ELEMENTAL("Элементаль"),
+	  FEY("Фей"),
+	  FIEND("Исчадие"),
+	  GIANT("Великан"),
+	  HUMANOID("Гуманойд"),
+	  MAGICAl_BEAST("Магический зверь"),
+	  OOZE("Тина"),
+	  OUTSIDER("Потустаронний"),
+	  PLANT("Растение"),
+	  DEVIL("Демон"),
+	  UNDEAD("Нежить"),
+	  VERMIN("Паразит");
+	  
+	  private String cyrilicName;
+	  
+	  private MonsterType(String cyrilicName)
+	  {
+	    this.cyrilicName = cyrilicName;
+	  }
+	  
+	  public String getCyrilicName()
+	  {
+	    return this.cyrilicName;
+	  }
 }
