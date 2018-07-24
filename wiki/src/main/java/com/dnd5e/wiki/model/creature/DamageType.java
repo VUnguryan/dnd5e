741446f8ac9c9a973518fd07cada3025b94004eb
VUnguryan <VUnguryan@VUnguryan.kiev.luxoft.com>
2018-07-24 17:18:09 +0300

    creature fix

--- wiki/src/main/java/com/dnd5e/wiki/model/creature/DamageType.java
+++ wiki/src/main/java/com/dnd5e/wiki/model/creature/DamageType.java
@@ -8,10 +8,13 @@ public enum DamageType {
 	ACID("кислота"),
 	SOUND("звук"),
 	NECTOTIC("некротическая энергия"),
+	PSYCHIC("психическая энергия"),
 	
 	CRUSHING("дробящий"),
 	PIERCING ("колющий"),
 	CHOPPING ("рубящий"),
+	PHYSICAL("дробящий, колющий и рубящий урон от немагических атак"),
+	NO_NOSILVER("дробящий, колющий и рубящий урон от немагических атак, а также от немагического оружия, которое при этом не посеребрено"),
 	;
 	private String cyrilicName;
 
@@ -20,5 +23,13 @@ public enum DamageType {
 	}
 	public String getCyrilicName() {
 		return cyrilicName;
+	}
+	public static DamageType parse(String damageTypeString) {
+		for (DamageType damageType : values()) {
+			if (damageType.cyrilicName.equals(damageTypeString)) {
+				return damageType;
+			}
+		}
+		return null;
 	}
 }
