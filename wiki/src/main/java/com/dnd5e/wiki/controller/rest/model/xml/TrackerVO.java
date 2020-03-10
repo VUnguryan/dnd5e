46a6d68b9544e059d447afc7215026ac8a7f1c9c
VUnguryan <VUnguryan@VUnguryan.kiev.luxoft.com>
2020-03-10 17:04:00 +0200

    -add tracker export to xml

--- wiki/src/main/java/com/dnd5e/wiki/controller/rest/model/xml/TrackerVO.java
+++ wiki/src/main/java/com/dnd5e/wiki/controller/rest/model/xml/TrackerVO.java
@@ -2,11 +2,18 @@ package com.dnd5e.wiki.controller.rest.model.xml;
 
 import javax.xml.bind.annotation.XmlElement;
 
+import com.dnd5e.wiki.model.hero.classes.Tracker;
+
 public class TrackerVO {
 	@XmlElement
 	String name;
 	@XmlElement
-	int value;
+	String value;
 	@XmlElement
 	String rest;
+	TrackerVO(Tracker tracker) {
+		this.name = tracker.getName();
+		this.value = tracker.getValue();
+		this.rest = "" + tracker.getRest().name().charAt(0);
+	}
 }
