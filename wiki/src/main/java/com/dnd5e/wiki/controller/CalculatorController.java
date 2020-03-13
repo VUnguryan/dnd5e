f14082c1c88e0eb9a384b42ab78f5e28c1972639
VUnguryan <VUnguryan@VUnguryan.kiev.luxoft.com>
2020-03-13 17:42:48 +0200

    - fix image sizes

--- wiki/src/main/java/com/dnd5e/wiki/controller/CalculatorController.java
+++ wiki/src/main/java/com/dnd5e/wiki/controller/CalculatorController.java
@@ -9,13 +9,19 @@ import com.dnd5e.wiki.model.hero.LifeStyle;
 import com.dnd5e.wiki.model.treasure.Rarity;
 
 @Controller
-@RequestMapping("/travel/idle")
-public class IdleController {
-	@GetMapping
+@RequestMapping("/calc")
+public class CalculatorController {
+	@GetMapping("/idle")
 	public String getIdleForm(Model model) {
 		model.addAttribute("lifeStyles", LifeStyle.values());
 		model.addAttribute("costs", new float[] {0, 0.1f, 0.2f, 1, 2, 4, 10 });
-		model.addAttribute("rarities", Rarity.values());
 		return "idle";
+	}
+
+	@GetMapping("/buyMagicThings")
+	public String getBuyMagicThingsForm(Model model) {
+		model.addAttribute("costs", new float[] {0, 0.1f, 0.2f, 1, 2, 4, 10 });
+		model.addAttribute("rarities", Rarity.values());
+		return "buyMagicThings";
 	}
 }
